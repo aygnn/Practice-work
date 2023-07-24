ece23d564da78fb5cc743b1c030a345a3839b1ab
aygnn <6yary5x@code.edu.az>
2023-07-24 17:47:15 +0300

    last

---
+++ project/home.js
@@ -0,0 +1,24 @@
+  var swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      rewind: true,
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 300,
+        modifier: 1,
+        slideShadows: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+    
+   
